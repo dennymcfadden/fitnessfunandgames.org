6137899f6f75573d17ed7bf9cb267f7d3d274959
Denny McFadden <dennymcfadden@gmail.com>
2017-11-26 15:01:04 -0500

    Implement JQuery ActiveNavigation plug-in
    https://github.com/Vitaa/ActiveNavigation

--- html/js/main.js
+++ html/js/main.js
@@ -1,4 +1,8 @@
+// Toggles visibility of 'nav' on click of 'nav-btn' //
 $('#nav-btn').click(function()
   {
     $('nav').slideToggle();
   });
+
+// Adds .active class to <a> tags with current URL //
+$(document).activeNavigation("#nav")
