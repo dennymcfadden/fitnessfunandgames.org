3c60e619d1c9f8038524b5dc91dad8e8662429d6
Denny McFadden <dennymcfadden@gmail.com>
2017-11-27 13:43:59 -0500

    Serve nav from nav.html

--- html/js/main.js
+++ html/js/main.js
@@ -1,11 +1,17 @@
-// Toggles visibility of 'nav' on click of 'nav-btn' //
-$('#nav-btn').click(function()
+// Replaces #nav-placeholder with nav.html //
+$(function()
   {
-    $('nav').slideToggle();
+  $("#nav-placeholder").load("nav.html");
   });
 
 // Adds .active class to <a> tags with current URL //
 $(document).ready(function()
   {
     $(document).activeNavigation("nav")
-  });	
+  });
+
+// Toggles visibility of 'nav' on click of 'nav-btn' //
+$('#nav-btn').click(function()
+  {
+    $('nav').slideToggle();
+  });
