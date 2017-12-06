224bedfdae87fa0ef48b7310dcfa6add1ee19893
Denny McFadden <dennymcfadden@gmail.com>
2017-12-05 19:31:33 -0500

    CSS edits & fixed JQuery bug

--- html/js/main.js
+++ html/js/main.js
@@ -1,22 +1,26 @@
+
 // Replaces #nav-placeholder with nav.html //
 $("#nav-placeholder").load("nav.html", function() {
 	// Adds .active class to <a> tags with current URL //
 	$(document).activeNavigation("nav");
+
+  const nav = $('nav');
+
 	// Toggles visibility of 'nav' on click of 'nav-btn' //
-	$('#nav-btn').click(function()
-    {
-    $('nav').slideToggle();
-    });
+	$('#nav-btn').click(function() {
+    nav.slideToggle();
+  });
 
   // When user resizes the window
   $(window).resize(function(){
     // Get the width of the page
     var width = $(window).width();
-    // If width > breakpoint and nav is hidden
-    if(width > 768 && nav.is(':hidden')) {
-      // Change display property to empty
-      nav.css('display','');
-   }
+      // If width > breakpoint and nav is hidden
+      if(width > 768 && nav.is(':hidden')) {
+        // Change display property to empty
+        nav.css('display','');
+     }
+  });
 });
 
-});
+
